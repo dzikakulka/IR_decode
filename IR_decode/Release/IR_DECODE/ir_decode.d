4bb493a9f8d64e85269bbbcddbbd63ef8f1bf5e3
dzikakulka <tofix112@gmail.com>
2015-12-29 23:02:11 +0100

    Usuniete smieci po-kompilacyjne

--- IR_decode/Release/IR_DECODE/ir_decode.d
+++
@@ -1,6 +0,0 @@
-IR_DECODE/ir_decode.o: ../IR_DECODE/ir_decode.c ../IR_DECODE/ir_decode.h \
- ../IR_DECODE/../makrau.h
-
-../IR_DECODE/ir_decode.h:
-
-../IR_DECODE/../makrau.h:
--- IR_decode/Release/main.d
+++
@@ -1,5 +0,0 @@
-main.o: ../main.c ../LCD/lcd44780.h ../IR_DECODE/ir_decode.h
-
-../LCD/lcd44780.h:
-
-../IR_DECODE/ir_decode.h:
